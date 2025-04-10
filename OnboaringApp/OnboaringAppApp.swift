daaa68788ba5cc1b2889e3ce93d23bea5ce9cc05
akotetsu <yuanliqu@gmail.com>
2025-04-10 20:28:59 +0900

    Initial Commit

---
+++ OnboaringApp/OnboaringAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboaringAppApp.swift
+//  OnboaringApp
+//
+//  Created by 原里駆 on 2025/04/10.
+//
+
+import SwiftUI
+
+@main
+struct OnboaringAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
